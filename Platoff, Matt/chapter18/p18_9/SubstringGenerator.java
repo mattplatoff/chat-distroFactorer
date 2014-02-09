ecc7877173fc26dd47a315292534a65a483139c2
mattplatoff <mattplatoff@gmail.com>
2014-02-09 14:45:46 -0500

    18.9 commit

--- Platoff, Matt/chapter18/p18_9/SubstringGenerator.java
+++ Platoff, Matt/chapter18/p18_9/SubstringGenerator.java
@@ -3,29 +3,40 @@ package p18_9;
 import java.util.ArrayList;
 
 public class SubstringGenerator {
-	static ArrayList<String> subs = new ArrayList<String>();
+	private static ArrayList<String> subs = new ArrayList<String>();
 	public static void main(String[] args) {
-		for (String s : getSubstrings("eat"))
-			System.out.println(s);
-		System.out.println(getSubstrings("eat").size());
+		System.out.println(getSubstrings("eat"));
+		
 	}
 
 	public static ArrayList<String> getSubstrings(String x) {
+//		
+//		if (x.length() == 0){
+//			subs.add("");
+//			
+//		return subs;
+//		}
+//		
+//		if (x.length()==1){
+//		subs.add(x);
+//			return getSubstrings(x.substring(0, x.length()-1));
+//		}
+//		else 
+//			
+//		{
+//		subs.add(x);
+//		return getSubstrings(x.substring(1));
+//		
+
 		
-		if (x.length() == 0){
-			subs.add("");
-			
+		for(int beg = 0; beg < x.length(); beg++)
+			for(int end = beg; end <=x.length(); end++)
+				subs.add(x.substring(beg, end));
+		
 		return subs;
-		}
-		else {
-		subs.add(x);
-		return getSubstrings(x.substring(0,x.length()-1));
-			
-		
 		}
 		
 		
 		
 
 	}
-}
