38da62014e25768f20582fff14db59689657344d
mattplatoff <mattplatoff@gmail.com>
2014-02-15 12:01:09 -0500

    modified hanoi

--- Platoff, Matt/chapter18/p18_13/Hanoi.java
+++ Platoff, Matt/chapter18/p18_13/Hanoi.java
@@ -1,17 +1,20 @@
 package p18_13;
 
+import java.util.Scanner;
+
 public class Hanoi {
 	public static void main(String[] args){
-		move(1,3,3);
+		Scanner kb=new Scanner(System.in);
+		System.out.println("enter first peg");
+		int p1=kb.nextInt();
+		System.out.println("enter target peg");
+		int p2=kb.nextInt();
+		System.out.println("enter num of disks");
+		int disks=kb.nextInt();
+		move(p1,p2,disks);
 	}
-	
-//	Move a disk from peg 1 to peg 3.
-//	Move a disk from peg 1 to peg 2.
-//	Move a disk from peg 3 to peg 2.
-//	Move a disk from peg 1 to peg 3.
-//	Move a disk from peg 2 to peg 1.
-//	Move a disk from peg 2 to peg 3.
-//	Move a disk from peg 1 to peg 3.
+
+
 	public static void move(int source, int target, int disks) {
 		int peg1=source,peg2=6-(source+target),peg3=target;
 		if (source > 3 || source < 1 || target < 1 || target > 3)
