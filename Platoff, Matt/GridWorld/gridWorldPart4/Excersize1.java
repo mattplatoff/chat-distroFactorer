73f8dae8bc8d690c555a488930f73ad68177740d
Matt Platoff <mattplatoff@gmail.com>
2014-02-25 13:43:38 -0500

    sheep commit

---
+++ Platoff, Matt/GridWorld/gridWorldPart4/Excersize1.java
@@ -0,0 +1,6 @@
+package gridWorldPart4;
+
+public class Excersize1 {
+
+	
+}
---
+++ Platoff, Matt/chapter_13/homeworkQuiz/SheepCritter.java
@@ -0,0 +1,60 @@
+package homeworkQuiz;
+
+import java.util.ArrayList;
+
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Location;
+
+public class SheepCritter extends Critter {
+private int wolfCount=0;
+public ArrayList<Actor> getActors(){
+	ArrayList<Actor> actors=new ArrayList<Actor>();
+	if (this.getGrid().get(this.getLocation().getAdjacentLocation(this.getDirection())) instanceof WolfCritter){
+		actors.add(this.getGrid().get(this.getLocation().getAdjacentLocation(this.getDirection())));
+	}
+	return actors;
+	
+}
+
+
+
+public void processActors(ArrayList<Actor> a){
+	if (a.get(0)!=null){
+		wolfCount++;
+		if (wolfCount==3)
+			this.removeSelfFromGrid();
+		if (this.getGrid().get(this.getLocation().getAdjacentLocation(this.getDirection()-Location.HALF_CIRCLE)).equals(null)){
+			this.moveTo(this.getLocation().getAdjacentLocation(this.getDirection()-Location.HALF_CIRCLE));
+		}
+		
+		}
+	else {
+		this.wolfCount=0;
+		return;}
+	}
+	public ArrayList<Location> getMoveLocations(){
+		ArrayList<Location> locs=new ArrayList<Location>();
+		if (this.getGrid().get(this.getLocation().getAdjacentLocation(this.getDirection()-Location.HALF_CIRCLE)).equals(null))
+		 locs.add(this.getLocation().getAdjacentLocation(this.getDirection()-Location.HALF_CIRCLE));
+		else 
+			locs=super.getMoveLocations();
+		return locs;
+		
+		
+	}
+public Location selectMoveLocations(ArrayList<Location> l){
+	if (this.getLocation().getAdjacentLocation(this.getDirection()-Location.HALF_CIRCLE) != null){
+		return this.getLocation().getAdjacentLocation(this.getDirection()-Location.HALF_CIRCLE);
+		
+	}
+	else 
+		return super.selectMoveLocation(l);
+		
+	
+	
+}
+}
+	
+	
+
