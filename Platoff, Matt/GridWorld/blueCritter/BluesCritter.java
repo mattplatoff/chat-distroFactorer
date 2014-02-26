273eaa71514fc4705f96a1e18dacd26876d347a1
Matt Platoff <mattplatoff@gmail.com>
2014-02-26 14:48:38 -0500

    yolo commit

---
+++ Platoff, Matt/GridWorld/blueCritter/BluesCritter.java
@@ -0,0 +1,43 @@
+package blueCritter;
+
+import java.util.ArrayList;
+
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Location;
+
+public class BluesCritter extends Critter {
+	/**
+	 * gets all actors with in a 10 block radius filters all but blue rocks and
+	 * returns an arraylist with rocks and returns them in an array list
+	 */
+	public ArrayList<Actor> getActors() {
+		return null;
+
+	}
+
+	/**
+	 * takes the locations of all blue rocks and puts them in an array list.
+	 * randomly selects a rock and
+	 */
+	public void processActors(ArrayList<Actor> actors) {
+
+	}
+/**
+ * 
+ */
+	public ArrayList<Location> getMoveLocations() {
+		return null;
+
+	}
+
+	public Location selectMoveLocation(ArrayList<Location> locs) {
+		return null;
+
+	}
+
+	public void makeMove(Location loc) {
+
+	}
+
+}
